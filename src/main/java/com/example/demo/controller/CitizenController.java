2718ca3a58f07928ce3044a17c4b846d1c8a66a5
aniket <dutta.aniket1572@gmail.com>
2026-03-31 10:19:33 +0530

    CitizenController updated

--- src/main/java/com/example/demo/controller/CitizenController.java
+++ src/main/java/com/example/demo/controller/CitizenController.java
@@ -1,37 +1,5 @@
 package com.example.demo.controller;
 
-<<<<<<< HEAD
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.model.Citizen;
-import com.example.demo.repository.CitizenRepository;
-
-import lombok.AllArgsConstructor;
-
-@RestController
-@RequestMapping("/api/citizens")
-@AllArgsConstructor
-public class CitizenController {
-
-	private CitizenRepository citizenRepository;
-
-	@PostMapping // http://localhost:9866/api/citizens
-	public ResponseEntity<Citizen> createCitizen(@RequestBody Citizen citizen) {
-		Citizen saved = citizenRepository.save(citizen);
-		return ResponseEntity.ok(saved);
-	}
-
-	@GetMapping("/{id}") // http://localhost:9866/api/citizens/1
-	public ResponseEntity<Citizen> getCitizen(@PathVariable Long id) {
-		return citizenRepository.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
-	}
-=======
 import com.example.demo.dto.*;
 import com.example.demo.service.CitizenService;
 import org.springframework.web.bind.annotation.*;
@@ -65,5 +33,5 @@ public class CitizenController {
     public CitizenProfileResp update(@PathVariable Long id, @RequestBody ProfileUpdateReq req) {
         return service.updateProfile(id, req);
     }
->>>>>>> origin/Vedasri
+
 }
